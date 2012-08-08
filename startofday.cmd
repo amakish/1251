8fb6431a5454c28b0d308d59f118e7690908b0c1
Aaron Makish <aaron.makish@gmail.com>
2012-08-08 09:24:48 -0400

    added startofday.cmd so that I wouldn't loose my work

---
+++ startofday.cmd
@@ -0,0 +1,7 @@
+c:
+
+cd "C:\Program Files (x86)\Apache Software Foundation\Apache2.2"
+
+rename htdocs htdocs.old
+
+mklink /D htdocs "t:\Web Dev\[School-#1251]\12 PROG1940 - PHP\[PHP - In Class Examples]\github"
