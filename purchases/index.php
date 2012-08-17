c261c5309fc121cefbcb7d00bf4af20a43ab25ca
amakish <aaron.makish@gmail.com>
2012-08-17 13:05:08 -0400

    added views to purchases app

--- purchases/index.php
+++ purchases/index.php
@@ -12,5 +12,23 @@ ActiveRecord\Config::initialize(function($cfg)
 			)
 	);
 });
+
+if(array_key_exists('submit', $_POST)){
+	$oPurchase = new Purchase();
+	
+	$oPurchase->date = $_POST['date'];
+	$oPurchase->purchase = $_POST['purchase'];
+	$oPurchase->price = $_POST['price'];
+	
+	$oPurchase->save();
+}
+
+
+if(array_key_exists('add', $_POST)){
+	include 'views/add.php';
+}
+else {
+	include 'views/list.php';
+}
+
 ?>
-<html><body>Hello World!</body></html>
