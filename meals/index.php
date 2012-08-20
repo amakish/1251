aeabfb3d9f3360a28af9bf4b0dba99149d2a7c77
amakish <aaron.makish@gmail.com>
2012-08-20 09:47:30 -0400

    Mid-Term 	Part 3 - 3) configure Database connection

--- meals/index.php
+++ meals/index.php
@@ -1,3 +1,20 @@
 <html><body>
 	Hello World!
 </body></html>
+
+<?php
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/mealss',
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
+
+?>
