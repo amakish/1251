8df169d13f86a09b5c859e4d7bb7681d9a93c038
Richard Hildred <rhildred@gmail.com>
2012-08-27 22:47:06 -0300

    made the model directory be 'model' instead of '.' sb what you need to get past the Fatal error: Class 'sktrstat' not found in C:\xampp\htdocs\1251\Assignment\views\sktrstatsview.php on line 5

--- Assignment/index.php
+++ Assignment/index.php
@@ -5,7 +5,7 @@
 
 	ActiveRecord\Config::initialize(function($cfg)
 	{
-		$cfg->set_model_directory('.');
+		$cfg->set_model_directory('model');
 		$cfg->set_connections(
 			array(
 					'development' => 'mysql://root:@localhost/nhl',
