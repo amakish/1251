384ec104bfbd409076b41e9bf678fa14e6cf1412
amakish <aaron.makish@gmail.com>
2012-08-30 12:59:25 -0400

    added socialmediasignin

---
+++ socialmediasignin/views/signin.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<form action='.' method="post">
+<input type="image" value='signin' name='action' src='views/VectorSocialMediaIcons/PNG/32px/google.png' alt='Sign In' /> 
+</form>
+</body>
+</html>
